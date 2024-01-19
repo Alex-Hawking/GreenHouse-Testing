4db0e49d70938d3a1571bcaf8829d327b3e9d6e7
Alex Hawking <alexhawking23@gmail.com>
2024-01-19 15:43:15 +0800

    reformat so that compiler is separate from actual code, creates temp dir etc in bdd folder

--- runner-docker/jest.config.js
+++ runner-docker/jest.config.js
@@ -1,11 +1,6 @@
-const path = require('path');
 const globals = require('./tests/GreenHouse')
 
 module.exports = {
   testTimeout: globals.testTimeout,
   verbose: globals.verbose,
-  moduleNameMapper: {
-    '^@Step/(.*)$': path.resolve(__dirname, 'tests/dist/pickle/step/$1'),
-    '^@Actions/(.*)$': path.resolve(__dirname, 'tests/dist/pickle/actions/$1'),
-  },
 };
---
+++ runner-docker/readme.txt
@@ -0,0 +1,3 @@
+This is the docker image for running the tests
+
+Will be used in the action, still ironing out how this will work, currently saves a video of tests
